aa3d8337c33fd004169a313fceb1519f6ce1cbc1
Larissa Giovanna <143462771+LarissaGiovanna@users.noreply.github.com>
2024-05-03 14:47:14 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,21 +1,42 @@
 function desc1() {
     let resposta1 = document.getElementById('resp1');
-    resposta1.style.display = 'block';
+    let imgDesc1 = document.getElementById('imgDesc1');
 
-    let imgDesc = document.getElementById('imgDesc1');
-    imgDesc.addEventListener('click', ()=>{
-        resposta1.style.display = 'none';
-    })
+    if (resposta1.style.display == 'none') {
+        resposta1.style.display = 'block';
+        
+
+    } else {
+        resposta1.style.display = "none";
+    }
 }
 function desc2() {
     let resposta2 = document.getElementById('resp2');
-    resposta2.style.display = 'block';
+    let imgDesc2 = document.getElementById('imgDesc2');
+
+    if (resposta2.display.style == 'none') {
+        resposta2.style.display = 'block';
+    } else {
+        resposta2.style.display = 'none'
+    }
 }
 function desc3() {
     let resposta3 = document.getElementById('resp3');
-    resposta3.style.display = 'block';
+    let imgDesc3 = document.getElementById('imgDesc3');
+
+    if(resposta3.style.display == 'none'){
+        resposta3.style.display = 'block';
+    }else{
+        resposta3.style.display = 'none';
+    }
 }
 function desc4() {
     let resposta4 = document.getElementById('resp4');
-    resposta4.style.display = 'block';
+    let imgDesc4 = document.getElementById('imgDesc4');
+
+    if(resposta4.style.display == 'none'){
+        resposta4.style.display = 'block';
+    }else{
+        resposta4.style.display = 'none';
+    }
 }
